f1c5a74cfce31f25bb5d2c8fd52014881e3585f0
Athly23 <andrewly06@gmail.com>
2020-03-22 21:31:07 -0400

    added pics and info with some css styling

--- api.js
+++ api.js
@@ -2,20 +2,49 @@ const url = "https://rickandmortyapi.com/api/character/";
 
 let gallery = document.querySelector(".characters");
 let array = document.querySelector(".character-pics");
-let info = document.querySelector("p")
+// let info = document.querySelector("p")
 let previous = document.querySelector(".left")
+let currentImage 
+let iterator = 0
+let imageArray
+let info = document.querySelector(".text-container")
+let para1 = document.querySelector(".para-1")
+let para2 = document.querySelector(".para-2")
+let para3 = document.querySelector(".para-3")
+let para4 = document.querySelector(".para-4")
+let para5 = document.querySelector(".para-5")
+
+    
+    let fetchRes = fetch(url) 
+    .then(res => res.json())
+    .then(res => {
+        imageArray = res.results
+        return imageArray
+    })
+    
+    imageArray=fetchRes 
+    
+    setTimeout(function(){
+        array.setAttribute('src', imageArray[iterator].image) 
+        para1.textContent = imageArray[iterator].name 
+        para2.textContent = imageArray[iterator].status
+        para3.textContent = imageArray[iterator].species
+        para4.textContent = imageArray[iterator].gender
+        
+
+    }, 200)
+    
+
+
+
+
 
 let handleClickLeft = (e) => {
     e.preventDefault()
-    fetch(url)
-        .then(res => res.json())
-        .then(res => {  
-           console.log("yay", res)
-            array.setAttribute('src', res.results[0].image)
-            
-        })
-
-        .catch(err => console.log("oops", err));
+    iterator--
+    if (iterator < 0) iterator = imageArray.length -1 
+    array.setAttribute('src', imageArray[iterator].image)
+    info.textContent = imageArray[iterator].name
 
     }
 
@@ -26,15 +55,16 @@ let next = document.querySelector(".right")
 
 let handleClickRight = (e) => {
     e.preventDefault()
-    fetch(url)
-        .then(res => res.json())
-        .then(res => {  
-        console.log("yay", res)
-        array.setAttribute('src', res.results[0].image)
+    iterator++
+    if (iterator > imageArray.length -1) iterator = 0;
+    array.setAttribute('src', imageArray[iterator].image)
+    para1.textContent = imageArray[iterator].name
+    para2.textContent = imageArray[iterator].status
+    para3.textContent = imageArray[iterator].species
+    para4.textContent = imageArray[iterator].gender
     
-})
-
-    .catch(err => console.log("oops", err));
+    
+    
 
 }
 
